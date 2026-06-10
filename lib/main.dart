53805d4aaf751d136bc43fe8e6c77b0167cd2275
Esrael Wendimu <keneanesrael@gmail.com>
2026-06-10 16:41:01 +0300

    additional responsive code

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:main/Carts/cartpage.dart';
-import 'package:main/login/CreateAccountView.dart';
-import 'package:main/cartpage2/cartpage2.dart';
-import 'package:main/categoriesPage/CategoriesView%20.dart';
-import 'package:main/createpost/createpost.dart';
-import 'package:main/dateandtime/date_time_view.dart';
-import 'package:main/managerpage/managerpage.dart';
-import 'package:main/nations/SearchNationsPage.dart';
-import 'package:main/postview/postview.dart';
-import 'package:main/savedpage/savedpage.dart';
 import 'package:main/startupView/startup_view.dart';
-void main()
-{
-  runApp(MyApp());
+import 'package:main/utils/mobile_web_wrapper.dart';
+
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return MobileWebWrapper(child: child ?? const SizedBox.shrink());
+      },
       home: const StartupView(),
     );
   }
